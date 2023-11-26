366e3043d9ba14f8f6c0e84ecba96208b9f4b5b0
GayathriCodes2 <gayathricodes2@gmail.com>
2023-11-26 12:09:49 +0530

    update banner

--- app/page.js
+++ app/page.js
@@ -1,10 +1,9 @@
 import Carousel from '@components/banner';
-import Card from '@components/card';
 import React from 'react';
 function page() {
   return (
     <div>
-    <Carousel/><Card/>
+    <Carousel/>
     </div>
   )
 }
