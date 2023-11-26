078601912b81355dd53064b5e7e88fde35b8becb
muthukumar <muthukumar.murugan@gove.co>
2023-11-26 11:33:00 +0530

    adding banner code

--- components/banner.jsx
+++ components/banner.jsx
@@ -1,11 +1,58 @@
-import React from 'react'
+import React from 'react';
+import Image from 'next/image';
+
+const yourWidth = 1200; 
+const yourHeight = 800; 
 
 function Carousel() {
   return (
-    <div>
-      
+    <div className="relative max-w-screen-md mx-auto">
+      <div className="carousel">
+        <input className="carousel-open" type="radio" id="carousel-1" name="carousel" aria-hidden="true" hidden />
+        <div className="carousel-item absolute opacity-0" style={{ height: '50%', width: '100%' }}>
+          <Image src="/1.jpg" alt="Image 1" width={yourWidth} height={yourHeight} className="w-full h-full object-cover" />
+        </div>
+        <label htmlFor="carousel-3" className="prev control-1 w-10 h-10 ml-2 md:ml-8 absolute cursor-pointer hidden text-2xl font-bold text-white hover:text-gray-900 leading-tight text-center bg-black bg-opacity-50 rounded-full opacity-50">‹</label>
+        <label htmlFor="carousel-2" className="next control-1 w-10 h-10 mr-2 md:mr-8 absolute cursor-pointer hidden text-2xl font-bold text-white hover:text-gray-900 leading-tight text-center bg-black bg-opacity-50 rounded-full opacity-50">›</label>
+
+        <input className="carousel-open" type="radio" id="carousel-2" name="carousel" aria-hidden="true" hidden />
+        <div className="carousel-item absolute opacity-0" style={{ height: '50%', width: '100%' }}>
+          <Image src="/2.jpg" alt="Image 2" width={yourWidth} height={yourHeight} className="w-full h-full object-cover" />
+        </div>
+        <label htmlFor="carousel-1" className="prev control-2 w-10 h-10 ml-2 md:ml-8 absolute cursor-pointer hidden text-2xl font-bold text-white hover:text-gray-900 leading-tight text-center bg-black bg-opacity-50 rounded-full opacity-50">‹</label>
+        <label htmlFor="carousel-3" className="next control-2 w-10 h-10 mr-2 md:mr-8 absolute cursor-pointer hidden text-2xl font-bold text-white hover:text-gray-900 leading-tight text-center bg-black bg-opacity-50 rounded-full opacity-50">›</label>
+
+        <input className="carousel-open" type="radio" id="carousel-3" name="carousel" aria-hidden="true" hidden />
+        <div className="carousel-item absolute opacity-0" style={{ height: '50%', width: '100%' }}>
+          <Image src="/3.jpg" alt="Image 3" width={yourWidth} height={yourHeight} className="w-full h-full object-cover" />
+        </div>
+        <label htmlFor="carousel-2" className="prev control-3 w-10 h-10 ml-2 md:ml-8 absolute cursor-pointer hidden text-2xl font-bold text-white hover:text-gray-900 leading-tight text-center bg-black bg-opacity-50 rounded-full opacity-50">‹</label>
+        <label htmlFor="carousel-4" className="next control-3 w-10 h-10 mr-2 md:mr-8 absolute cursor-pointer hidden text-2xl font-bold text-white hover:text-gray-900 leading-tight text-center bg-black bg-opacity-50 rounded-full opacity-50">›</label>
+
+        <input className="carousel-open" type="radio" id="carousel-4" name="carousel" aria-hidden="true" hidden />
+        <div className="carousel-item absolute opacity-0" style={{ height: '50%', width: '100%' }}>
+          <Image src="/4.jpg" alt="Image 4" width={yourWidth} height={yourHeight} className="w-full h-full object-cover" />
+        </div>
+        <label htmlFor="carousel-3" className="prev control-4 w-10 h-10 ml-2 md:ml-8 absolute cursor-pointer hidden text-2xl font-bold text-white hover:text-gray-900 leading-tight text-center bg-black bg-opacity-50 rounded-full opacity-50">‹</label>
+        <label htmlFor="carousel-1" className="next control-4 w-10 h-10 mr-2 md:mr-8 absolute cursor-pointer hidden text-2xl font-bold text-white hover:text-gray-900 leading-tight text-center bg-black bg-opacity-50 rounded-full opacity-50">›</label>
+
+        <ol className="carousel-indicators">
+          <li className="inline-block mr-3">
+            <label htmlFor="carousel-1" className="carousel-bullet cursor-pointer block text-4xl text-white hover:text-gray-900">•</label>
+          </li>
+          <li className="inline-block mr-3">
+            <label htmlFor="carousel-2" className="carousel-bullet cursor-pointer block text-4xl text-white hover:text-gray-900">•</label>
+          </li>
+          <li className="inline-block mr-3">
+            <label htmlFor="carousel-3" className="carousel-bullet cursor-pointer block text-4xl text-white hover:text-gray-900">•</label>
+          </li>
+          <li className="inline-block mr-3">
+            <label htmlFor="carousel-4" className="carousel-bullet cursor-pointer block text-4xl text-white hover:text-gray-900">•</label>
+          </li>
+        </ol>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Carousel
+export default Carousel;
--- components/header.jsx
+++ components/header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
 
     return (
-        <header className="bg-green-600 text-white p-4 flex flex-col md:flex-row justify-between items-center">
+        <header className="bg-green-600 mt-2 mx-2 text-white p-4 flex flex-col md:flex-row justify-between items-center rounded-full">
             {/* Logo and Navigation Menu (Desktop) */}
             <div className="flex items-center space-x-4">
                 <div className="flex items-center space-x-2">
@@ -19,45 +19,45 @@ const Header = () => {
             <div className="flex items-center space-x-4">
                 <nav className="hidden md:flex items-center space-x-4 ml-auto ">
                     <a href="/about-us" className="nav-link hover:bg-green-500 transition-all rounded p-2">Who We Are</a>
-                    <a href="/contact-us" className="nav-link  hover:bg-green-500 transition-all p-2">Get in Touch</a>
-                    <a href="/our-cultivated-products" className="nav-link  hover:bg-green-500 transition-all p-2">Our Harvested Goods</a>
-                    <a href="/famous-products" className="nav-link  hover:bg-green-500 transition-all p-2">Signature Local Delicacies</a>
+                    <a href="/contact-us" className="nav-link  hover:bg-green-500 transition-all rounded p-2">Get in Touch</a>
+                    <a href="/our-cultivated-products" className="nav-link  hover:bg-green-500 transition-all rounded p-2">Our Harvested Goods</a>
+                    <a href="/famous-products" className="nav-link  hover:bg-green-500 transition-all rounded p-2">Signature Local Delicacies</a>
                 </nav>
             </div>
 
             {/* Search Bar */}
             <div className="flex">
-                
-            <div className="md:hidden ml-0 self-end">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-6 cursor-pointer"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    onClick={() => setMobileMenuOpen(!isMobileMenuOpen)}
-                >
-                    <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M4 6h16M4 12h16m-7 6h7"
+
+                <div className="md:hidden ml-0 self-end">
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-6 w-6 cursor-pointer"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        onClick={() => setMobileMenuOpen(!isMobileMenuOpen)}
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M4 6h16M4 12h16m-7 6h7"
+                        />
+                    </svg>
+                </div>
+                <div className='items-center space-x-6'>
+                    <input
+                        type="text"
+                        placeholder="Search"
+                        className="px-4 py-1 border border-white rounded lg:w-40 transition-all duration-300 ease-in-out to-black focus:outline-none focus:border-green-400"
                     />
-                </svg>
-            </div>
-                <div className='items-center space-x-2'>
-                <input
-                    type="text"
-                    placeholder="Search"
-                    className="px-4 py-1 border border-white rounded lg:w-40 transition-all duration-300 ease-in-out to-black focus:outline-none focus:border-green-400"
-                />
-                <button className="bg-white text-gray-800 px-2 py-1 rounded">Search</button>
-</div>
-                 
+                    <button className="bg-white text-gray-800 px-2 py-1 rounded">Search</button>
+                </div>
+
             </div>
 
             {/* Mobile Menu Icon (Right-aligned) */}
-           
+
 
             {/* Navigation Menu (Mobile) */}
             {isMobileMenuOpen && (
