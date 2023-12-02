6c7e485c5069ba775a6abd23125fe135073d19b6
GayathriCodes2 <gayathricodes2@gmail.com>
2023-12-03 02:29:08 +0530

    Updated home page card

--- components/homePageListCard.jsx
+++ components/homePageListCard.jsx
@@ -1,9 +1,9 @@
 import Image from 'next/image';
-
+import Link from 'next/link';
 const HomepageCard = ({ data }) => {
   return (
-    <div className="shadow-lg border rounded-md overflow-hidden lg:p-4 lg:m-6 m-2 p-1 bg-slate-100">
-
+    <div className="shadow-lg border rounded-md overflow-hidden lg:p-4 lg:m-6 m-2 p-2 bg-slate-100">
+      <Link href={data.productUrl}>
       <div className="relative h-48 w-full">
         <Image
           src={data.imageUrl}
@@ -21,6 +21,7 @@ const HomepageCard = ({ data }) => {
           <p className="lg:text-lg font-bold text-xs text-green-500">${data.price}</p>
         </div>
       </div>
+      </Link>
     </div>
   );
 };
