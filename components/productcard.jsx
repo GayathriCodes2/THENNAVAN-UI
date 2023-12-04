baba2921a8004e835023076db1b0b6df7c8740ca
GayathriCodes2 <gayathricodes2@gmail.com>
2023-12-04 22:26:14 +0530

    updated product card

--- components/productcard.jsx
+++ components/productcard.jsx
@@ -13,16 +13,15 @@ const ProductCard = ({ imageUrl, heading, description, price, buyNowLink }) => {
       {/* Middle part: Heading and Description */}
       <div className="flex-grow lg:mx-4 mx-1">
         <h2 className="lg:text-xl text-lg font-semibold">{heading}</h2>
-        <p className="text-gray-600 text-xs lg:text-xl ml-2">{description}</p>
+        <p className="text-gray-600 text-xs lg:text-xl">{description}</p>
       </div>
 
       {/* Right side: Price and Buy Now button */}
       <div className="flex flex-col items-end">
         <p className="text-lg font-semibold">Rs.{price}</p>
         <Link href={buyNowLink} className='self-end lg:mt-4 mt-2'>
-          <button className="w-full px-1 lg:px-4 py-2 bg-white border-green-500 text-green-500 rounded-md focus:outline-none border hover:bg-green-500 hover:text-white transition duration-300 ease-in-out text-xl sm:text-xs">
-            Pre Book
-          </button>
+        <button className="w-full px-1 lg:px-2 py-2 bg-white border-green-500 text-green-500 rounded-md focus:outline-none border hover:bg-green-500 hover:text-white transition duration-300 ease-in-out text-sm sm:text-xs">
+          Pre Book</button>
         </Link>
       </div>
     </div>
