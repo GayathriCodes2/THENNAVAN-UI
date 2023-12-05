ed32b05b3441f388e06292a3db923854c9714c7a
muthukumar <muthukumar.murugan@gove.co>
2023-12-06 00:29:53 +0530

    Updated header component

--- components/header.jsx
+++ components/header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
             {/* Logo and Navigation Menu (Desktop) */}
             <div className="flex items-center space-x-4">
                 <div className="flex items-center space-x-2">
-                    <img src="/cultivate.png" alt="Logo" className="h-8 w-8"/>
+                    <img src="/cultivate.png" alt="Logo" className="h-8 w-8" />
                     <span className="text-lg font-bold">Thennavan</span>
                 </div>
             </div>
@@ -18,25 +18,25 @@ const Header = () => {
             {/* Navigation Menu (Desktop) */}
             <div className="flex items-center space-x-4">
                 <nav className="hidden md:flex items-center space-x-4 ml-auto ">
-                    <a href="/about-us" className="nav-link hover:bg-green-500 transition-all rounded p-2">About-us</a>
+                    <a href="/fromfarmersproducts" className="nav-link  hover:bg-green-500 transition-all rounded p-2">Our Harvested Goods</a>
+                    <a href="/tasteofourlandproducts" className="nav-link  hover:bg-green-500 transition-all rounded p-2">Signature Local Delicacies</a>
                     <a href="/sellwithus" className="nav-link  hover:bg-green-500 transition-all rounded p-2">Sell With Us</a>
-                    <a href="/our-cultivated-products" className="nav-link  hover:bg-green-500 transition-all rounded p-2">Our Harvested Goods</a>
-                    <a href="/famous-products" className="nav-link  hover:bg-green-500 transition-all rounded p-2">Signature Local Delicacies</a>
+                    <a href="/about-us" className="nav-link hover:bg-green-500 transition-all rounded p-2">About-us</a>
                 </nav>
             </div>
 
             {/* Search Bar */}
             <div className="flex">
-                
-            <div className="md:hidden ml-0 self-end">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor" onClick={() => setMobileMenuOpen(!isMobileMenuOpen)}>
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"/>
-                </svg>
-            </div>
+
+                <div className="md:hidden ml-0 self-end">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor" onClick={() => setMobileMenuOpen(!isMobileMenuOpen)}>
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
+                    </svg>
+                </div>
                 <div className='items-center space-x-2 flex'>
-                <input type="text" placeholder="Search" className="px-4 py-1 border flex items-center text-black border-white rounded-full lg:w-60 transition-all duration-300 ease-in-out to-black focus:outline-none focus:border-green-400"/>
-                <button className="bg-white text-gray-800 px-2 py-1 rounded-full">
-                    <svg    xmlns="http://www.w3.org/2000/svg"
+                    <input type="text" placeholder="Search" className="px-4 py-1 border flex items-center text-black border-white rounded-full lg:w-60 transition-all duration-300 ease-in-out to-black focus:outline-none focus:border-green-400" />
+                    <button className="bg-white text-gray-800 px-2 py-1 rounded-full">
+                        <svg xmlns="http://www.w3.org/2000/svg"
                             viewBox="0 0 20 20"
                             fill="currentColor"
                             className="h-5 w-5 my-1">
@@ -45,8 +45,8 @@ const Header = () => {
                                 d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
                                 clipRule="evenodd" />
                         </svg></button>
-</div>
-                
+                </div>
+
             </div>
 
             {/* Mobile Menu Icon (Right-aligned) */}
@@ -55,10 +55,10 @@ const Header = () => {
             {/* Navigation Menu (Mobile) */}
             {isMobileMenuOpen && (
                 <div className="md:hidden flex flex-col items-center space-y-2">
-                    <a href="/about-us" className="nav-link hover:bg-green-500 transition-all p-2">Who We Are</a>
-                    <a href="/sellwithus" className="nav-link  hover:bg-green-500 transition-all p-2">Sell With Us</a>
                     <a href="/our-cultivated-products" className="nav-link  hover:bg-green-500 transition-all p-2">Our Harvested Goods</a>
                     <a href="/famous-products" className="nav-link  hover:bg-green-500 transition-all p-2">Signature Local Delicacies</a>
+                    <a href="/sellwithus" className="nav-link  hover:bg-green-500 transition-all p-2">Sell With Us</a>
+                    <a href="/about-us" className="nav-link hover:bg-green-500 transition-all p-2">About-us</a>
                 </div>
             )}
         </header>
