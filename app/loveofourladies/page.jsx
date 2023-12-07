5277e7429519f9a7f1aca7ea49ca6725cde8a9b9
GayathriCodes2 <gayathricodes2@gmail.com>
2023-12-08 01:12:49 +0530

    Added Products

--- app/loveofourladies/page.jsx
+++ app/loveofourladies/page.jsx
@@ -180,7 +180,7 @@ const Page = () => {
   return (
     <div>
       <h1 className="text-2xl font-semibold my-4">From Farmers</h1>
-      <img src='/From-Farmers-All-Product-page-image.png' alt="Leelavathi" className="h-96 w-96 lg:hidden mx-auto" />
+      <img src='/download-removebg-preview.png' alt="Temple Image" className="h-96 w-96 lg:hidden mx-auto" />
       {products.map((product) => (
         <ProductCard key={product.id} {...product} />
       ))}
