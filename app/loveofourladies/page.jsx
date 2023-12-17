a5dcf3697f84ad8f499bf0956543347cf940e894
GayathriCodes2 <gayathricodes2@gmail.com>
2023-12-18 00:30:25 +0530

    updated product card to fetch data dynamically from backend

--- app/loveofourladies/page.jsx
+++ app/loveofourladies/page.jsx
@@ -10,7 +10,7 @@ const Page = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://localhost:3002/loveofourladies/loveofourladiess');
+        const response = await axios.get('https://thennavan.onrender.com/loveofourladies/loveofourladiess');
         setProducts(response.data);
       } catch (error) {
         console.error('Error fetching data:', error);
--- app/tasteofourlandproducts/page.jsx
+++ app/tasteofourlandproducts/page.jsx
@@ -10,7 +10,7 @@ const Page = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('http://localhost:3002/tasteofourland/tasteofourlands');
+        const response = await axios.get('https://thennavan.onrender.com/tasteofourland/tasteofourlands');
         setProducts(response.data);
       } catch (error) {
         console.error('Error fetching data:', error);
